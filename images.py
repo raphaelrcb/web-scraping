efc619008b2b42ad197dd5c97d492e3c211b5ab5
raphaelrcb <raphaelrcbarbosa@gmail.com>
2024-10-08 00:31:55 -0300

    smaller features

--- images.py
+++ images.py
@@ -4,35 +4,42 @@ import json
 import re
 from PIL import Image
 from io import BytesIO
+import os
 
-from main import item_href
+def ImageSearch():
 
-search = input("Procure por: ")
-#search = "vox machina"
-params = {"q": search}
-r = requests.get("http://www.bing.com/images/search", params=params)
+    search = input("Procure por: ")
+    #search = "vox machina"
+    if search == "":
+        quit()
+    params = {"q": search}
+    r = requests.get("http://www.bing.com/images/search", params=params)
 
-soup = BeautifulSoup(r.text, "html.parser")
+    dir_name = search.replace(" ", "_").lower()
 
-links = soup.findAll("a", {"class": "iusc"})
-#print(links)
-for item in links:
-    print("\n")
-    try:
-        m = json.loads(item["m"])
-        media_url = m["murl"]
-        media_title = m["murl"].split("/")[-1]
-        media_title = re.split(r'[/?&.]', media_title)[0]
-        print(item)
-        print(media_url)
-        print(media_title)
-        img_obj = requests.get(media_url)
-        img = Image.open(BytesIO(img_obj.content))
-        print(img.format)
-        img.save("./scraped_images/" + media_title + "." + img.format.lower())
-    except FileNotFoundError:
-        print("Erro em acessar url da imagem")
-    except IOError:
-        print("Um erro ocorreu ao abrir o arquivo")
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
 
+    soup = BeautifulSoup(r.text, "html.parser")
 
+    links = soup.findAll("a", {"class": "iusc"})
+    for item in links:
+        try:
+            m = json.loads(item["m"])
+            media_url = m["murl"]
+            media_title = m["murl"].split("/")[-1]
+            media_title = re.split(r'[/?&.]', media_title)[0]
+            print(media_url)
+            print(media_title)
+            img_obj = requests.get(media_url)
+            img = Image.open(BytesIO(img_obj.content))
+            img.save("./" + dir_name + "/" + media_title + "." + img.format.lower())
+        except FileNotFoundError:
+            print("Erro em acessar url da imagem")
+        except IOError:
+            print("Um erro ocorreu ao abrir o arquivo")
+        except:
+            print("Erro nao identificado")
+    ImageSearch()
+
+ImageSearch()
